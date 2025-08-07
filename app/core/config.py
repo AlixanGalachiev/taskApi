22dcf22818546615ab9f01599df7f3785af11f4c
AlixanGalachiev <all.gall1@mail.ru>
2025-08-07 09:52:48 +0300

    updated settings class, now it's work with different .env files

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,23 @@
+import os
 from functools import lru_cache
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+	ENV: str = "dev"
+
 	DB_HOST: str
 	DB_PORT: int
 	DB_USER: str
 	DB_PASS: str
 	DB_NAME: str
 
-
-	DB_HOST_TEST: str
-	DB_PORT_TEST: str
-	DB_USER_TEST: str
-	DB_PASS_TEST: str
-	DB_NAME_TEST: str
-
 	JWT_AUTH_KEY: str
+
+	class Config:
+		# Динамически выбираем .env-файл в зависимости от переменной ENV
+		env_file = f".env.{os.getenv('ENV', 'dev')}"
+		env_file_encoding = 'utf-8'
+
 
 	@property
 	def DATABASE_URL(self) -> str:
@@ -31,23 +33,6 @@ class Settings(BaseSettings):
 			f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 		)
 
-
-	@property
-	def DATABASE_URL_TEST(self) -> str:
-		return (
-			f"postgresql+asyncpg://{self.DB_USER_TEST}:{self.DB_PASS_TEST}"
-			f"@{self.DB_HOST_TEST}:{self.DB_PORT_TEST}/{self.DB_NAME_TEST}"
-		)
-
-	@property
-	def DATABASE_URL_SYNC_TEST(self) -> str:
-		return (
-			f"postgresql+psycopg2://{self.DB_USER_TEST}:{self.DB_PASS_TEST}"
-			f"@{self.DB_HOST_TEST}:{self.DB_PORT_TEST}/{self.DB_NAME_TEST}"
-		)
-
-	class Config:
-		env_file = ".env"
 
 @lru_cache()
 def get_settings():
